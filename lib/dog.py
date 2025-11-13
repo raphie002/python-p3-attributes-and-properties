e1e263e61f6df4a95c6347b28ec2e2e045c6a6ac
Raphael Njoroge <raphael.njoroge@student.moringaschool.com>
2025-11-13 09:43:17 +0300

    First commit

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        name_error_message = "Name must be string between 1 and 25 characters."
+        
+        is_name_valid = False
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            is_name_valid = True
+            self.name = name
+        elif name is not None:
+            print(name_error_message)
+            self.name = None
+        else:
+            self.name = None
+            
+        breed_error_message = "Breed must be in list of approved breeds."
+        
+        if breed and breed not in APPROVED_BREEDS:
+            print(breed_error_message)
+            self.breed = None
+        elif breed:
+            self.breed = breed
+        else:
+            self.breed = None
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+
+        name_error_message = "Name must be string between 1 and 25 characters."
+
+        is_name_valid = False
+        if isinstance(name, str) and (1 <= len(name) <= 25):
+            is_name_valid = True
+            self.name = name.title() 
+        elif name is not None:
+            print(name_error_message)
+            self.name = None 
+        else:
+            self.name = None 
+            
+        job_error_message = "Job must be in list of approved jobs."
+
+        if job and job not in APPROVED_JOBS:
+            print(job_error_message)
+            self.job = None
+        elif job:
+            self.job = job
+        else:
+            self.job = None
